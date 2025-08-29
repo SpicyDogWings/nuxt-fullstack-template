6cba97844a6a93d3e7de747deda5a36d73237494
spicydogwings <spaceprotondev@proton.me>
2025-08-29 07:32:28 -0600

    fix(ui): cambio en el problema del toogleColorMode en ssr mode

--- app/components/Switches/DarkMode.vue
+++ app/components/Switches/DarkMode.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-const colorMode = useColorMode();
+import { computed } from 'vue';
+import { useColorMode } from '#imports';
 
+const colorMode = useColorMode();
 const isDark = computed({
-  get: () => colorMode.preference === "dark",
+  get: () => colorMode.preference === 'dark',
   set: (value: boolean) => {
-    if (value && colorMode.value === 'dark') {
-      colorMode.preference = "system";
-    } else {
-      colorMode.preference = value ? "dark" : "light";
-    }
+    colorMode.preference = value ? 'dark' : 'light';
   },
 });
 </script>
 
 <template>
-  <USwitch
-    v-model="isDark"
-    unchecked-icon="lucide:sun"
-    checked-icon="lucide:moon"
-  />
+  <ClientOnly>
+    <USwitch
+      v-model="isDark"
+      unchecked-icon="lucide:sun"
+      checked-icon="lucide:moon"
+    />
+  </ClientOnly>
 </template>
